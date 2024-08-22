2cedd7ea725e2445c958fa4e5c325fe2394cde14
yasnogueir4 <00001124954454SP@al.educacao.sp.gov.br>
2024-08-22 12:52:52 -0300

    adicionando arquivos

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let cor
+let posicaoHorizontal; // x
+let posicaoVertical; //y
+
+function setup() {
+  createCanvas(400,400);
+  background(color(100,0,0)),
+    cor= color(random(0,255));random(0,255),random(0,255));
+  posicaoHorizental=200;
+  posicaoVertical=200;
+  
+  
+  
+  function draw() {
+    
+    fill(cor);
+circle(posicaoHorizontal,posicaoVertical,50);
+    
+    if (mouseX > posicaoHorizontal) }
+    posicaoHorizontal= posicaoHorizontal
+  }
+  
+  if (mouseX > posicaoHorizontal) }
+  posicaoHorizontal= posicaoHorizontal
+}
+
+if (mouseY < posicaoVertical) {}
